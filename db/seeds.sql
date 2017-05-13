a0d10300dddeb832bb0ca850dcd59d861826c5ac
JPSauSara <jpsausara@users.noreply.github.com>
2017-05-13 08:40:24 -0500

    create: database records

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- INSERT INTO burgers (burger_name, devoured) VALUES ("Big Kahuna", true)
+-- INSERT INTO burgers (burger_name) VALUES ("Big Kahuna") will ret. default false
+
+INSERT INTO burgers (burger_name) VALUES ("Big Kahuna");
+INSERT INTO burgers (burger_name) VALUES ("Krusty Burger");
+INSERT INTO burgers (burger_name) VALUES ("Whataburger");
+
---
+++ models/burger.js
@@ -0,0 +1,9 @@
+// Here is where you setup a model for how to interface with the database
+
+var orm = require('../config/orm.js');
+
+var burger = {
+
+};
+
+module.exports = burger;
